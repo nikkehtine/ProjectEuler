846a6ebe6d2f8f689511a01d342a88220636c48c
Nikkeh <nikkehtine@int.pl>
2023-09-04 18:16:17 +0200

    Problem 4: optimize palindrome generator

--- solutions/4. largest palindrome product.py
+++ solutions/4. largest palindrome product.py
@@ -12,9 +12,8 @@ def is_palindrome(input):
 
 def gen_palindromes():
     palindromes = []
-    # This is going to be unoptimized for now
-    for x in range(100, 999):
-        for y in range(100, 999):
+    for x in range(100, 1000):
+        for y in range(x, 1000):
             product = x * y
             if is_palindrome(str(product)):
                 palindromes.append(product)
