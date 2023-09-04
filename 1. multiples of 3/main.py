ee0c01a8e32acee4acf1a122ef1a75235acbc5ce
Nikkeh <nikkehtine@int.pl>
2023-09-04 14:16:40 +0200

    Problem 1: Add comment

--- 1. multiples of 3/main.py
+++ 1. multiples of 3/main.py
@@ -1,7 +1,11 @@
+# Multiples of 3 or 5
+# https://projecteuler.net/problem=1
+
 def multiples(limit):
     multiples = []
     for i in range(limit):
         if i % 3 == 0 or i % 5 == 0:
             multiples.append(i)
     return multiples
+
 print(sum(multiples(1000)))
