40105283ca471983e4963bea3220e45302ab17d3
Nikkeh <nikkehtine@int.pl>
2023-09-04 18:10:54 +0200

    Problem 4: finish the script

--- solutions/4. largest palindrome product.py
+++ solutions/4. largest palindrome product.py
@@ -8,3 +8,24 @@ def is_palindrome(input):
         half_1 += input[i]
         half_2 += input[-i - 1]
     return True if half_1 == half_2 else False
+
+
+def gen_palindromes():
+    palindromes = []
+    # This is going to be unoptimized for now
+    for x in range(100, 999):
+        for y in range(100, 999):
+            product = x * y
+            if is_palindrome(str(product)):
+                palindromes.append(product)
+    return palindromes
+
+
+def largest_palindrome():
+    largest = 0
+    for i in gen_palindromes():
+        largest = i if i > largest else largest
+    return largest
+
+
+print(largest_palindrome())
