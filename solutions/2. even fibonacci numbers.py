c20662cdcc0f8e76bd90797630c645fb076d2ef8
Nikkeh <nikkehtine@int.pl>
2023-09-04 19:32:10 +0200

    Problem 2: optimize the code

--- solutions/2. even fibonacci numbers.py
+++ solutions/2. even fibonacci numbers.py
@@ -1,27 +1,23 @@
 # Even Fibonacci numbers
 # https://projecteuler.net/problem=2
 
+max_val = 4000000
+
 
 def fib_gen(limit):
-    seq = [0, 1]
-    while True:
+    seq, next_num = [0, 1], 1
+    while next_num <= limit:
         next_num = seq[-2] + seq[-1]
-        if next_num <= limit:
-            seq.append(next_num)
-        else:
-            break
+        seq.append(next_num) if next_num <= limit else seq
     return seq
 
 
 def even_vals(list):
     even_list = []
     for i in list:
-        if i % 2 == 0:
-            even_list.append(i)
+        even_list.append(i) if i % 2 == 0 else i
     return even_list
 
 
-max_val = 4000000
 sum_of_even_vals = sum(even_vals(fib_gen(max_val)))
-
 print(sum_of_even_vals)
