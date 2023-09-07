9d1c7a5cb0119e892d6842fe7f2cf37e2544c2f9
Nikkeh <nikkehtine@int.pl>
2023-09-07 18:12:43 +0200

    #2 (JS): Add solution

---
+++ javascript/2. even fibonacci numbers.js
@@ -0,0 +1,23 @@
+// Even Fibonacci numbers
+// https://projecteuler.net/problem=2
+
+function fiboEvenSum(n) {
+    let a, b, next, sum;
+    a = b = 1;
+    next = sum = 0;
+    while (true) {
+        next = a + b;
+        if (next > n) {
+            break;
+        }
+        if (next % 2 == 0) {
+            sum += next;
+        }
+        a = b;
+        b = next;
+    }
+
+    return sum;
+}
+
+console.log(fiboEvenSum(4000000));
