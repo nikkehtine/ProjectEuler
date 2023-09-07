aa0dad44435caef92675c5b604a0529a7e83df6e
Nikkeh <nikkehtine@int.pl>
2023-09-07 17:46:47 +0200

    Add .prettierrc.yaml

--- javascript/1. multiples of 3 or 5.js
+++ javascript/1. multiples of 3 or 5.js
@@ -2,14 +2,14 @@
 // https://projecteuler.net/problem=1
 
 function multiplesOf3and5(number) {
-  const multiples = [];
-  for (let i = 3; i < number; i++) {
-    if (i % 3 == 0 || i % 5 == 0) {
-      multiples.push(i);
+    const multiples = [];
+    for (let i = 3; i < number; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            multiples.push(i);
+        }
     }
-  }
-  let sum = multiples.reduce((acc, value) => acc + value);
-  return sum;
+    let sum = multiples.reduce((acc, value) => acc + value);
+    return sum;
 }
 
 console.log(multiplesOf3and5(1000));
