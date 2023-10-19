b990861e2d60c7f14b847514bd20e3d666bcb5ab
nikkehtine <nikkehtine@int.pl>
2023-10-19 18:16:28 +0200

    #3: use stdbool header, fix types

--- c/3. largest prime factor.c
+++ c/3. largest prime factor.c
@@ -2,19 +2,19 @@
 // https://projecteuler.net/problem=3
 
 #include <math.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef enum { false, true } bool;
 const long long big_int = 600851475143;
 
-bool isPrime(long n) {
+bool isPrime(int n) {
     if (n < 2 || (n % 2 == 0 && n != 2)) {
         return false;
     } else if (n == 2) {
         return true;
     }
-    const long long root = sqrt(n);
+    const int root = sqrt(n);
     for (int i = 3; i <= root; i += 2) {
         if (n % i == 0) {
             return false;
