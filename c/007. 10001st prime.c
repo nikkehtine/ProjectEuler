fcd57db09d7018286b6ffbb2aaf436beb24de3c2
nikkehtine <nikkehtine@int.pl>
2024-11-07 13:37:09 +0100

    #7 (C): Add header

--- c/007. 10001st prime.c
+++ c/007. 10001st prime.c
@@ -5,20 +5,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-bool isPrime(int n) {
-    if (n < 2) {
-        return false;
-    } else if (n > 2 && n % 2 == 0) {
-        return false;
-    }
-    const int root = sqrt(n);
-    for (int i = 3; i <= root; i += 2) {
-        if (n % i == 0) {
-            return false;
-        }
-    }
-    return true;
-}
+#include "lib/prime.h"
 
 long long nthPrime(int target) {
     int n;
